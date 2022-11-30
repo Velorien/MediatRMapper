cea721bf1f7b5fd76549c491f46c6a73fe7a0eda
Velorien <k.miczkowski@outlook.com>
2022-11-30 15:27:12 +0100

    Added cocona command line tools

--- MediatRMapper/Program.cs
+++ MediatRMapper/Program.cs
@@ -1,79 +1,118 @@
-﻿// See https://aka.ms/new-console-template for more information
-
+﻿using System.Text.RegularExpressions;
 using MediatRMapper;
 using Microsoft.Build.Locator;
 using Microsoft.CodeAnalysis;
 using Microsoft.CodeAnalysis.CSharp;
 using Microsoft.CodeAnalysis.CSharp.Syntax;
 using Microsoft.CodeAnalysis.MSBuild;
+using Cocona;
 
 MSBuildLocator.RegisterDefaults();
 
-var handlers = new List<HandlerInfo>();
-var senders = new Dictionary<string, List<string>>();
+CoconaApp.Run(RunMapper);
 
-var workspace = MSBuildWorkspace.Create();
-var sln = await workspace.OpenSolutionAsync(args[0]);
-foreach (var project in sln.Projects)
+async Task RunMapper(string? solutionPath, bool verbose, bool stripNamespaces = true)
 {
-    Console.WriteLine($"Processing {project.Name}");
-    var compilation = await project.GetCompilationAsync();
-
-    if (compilation is null) continue;
+    var namespaceRegex = new Regex(@"\w+\.");
+    var handlers = new List<HandlerInfo>();
+    var senders = new Dictionary<string, List<string>>();
+    var workspace = MSBuildWorkspace.Create();
     
-    foreach (var tree in compilation.SyntaxTrees)
+    string GetHandler(string type)
     {
-        var root = await tree.GetRootAsync();
-        foreach (var typeDeclaration in root.DescendantNodes().OfType<ClassDeclarationSyntax>())
+        var handler = handlers.FirstOrDefault(x => x.Response == type);
+        if (handler is not null) return handler.Name;
+        handler = handlers.FirstOrDefault(x => x.Request == type);
+
+        return handler?.Name ?? "Unknown";
+    }
+
+    string GetRequest(string type)
+    {
+        var handler = handlers!.FirstOrDefault(x => x.Response == type);
+        return handler?.Request ?? type;
+    }
+
+    string FormatTypeName(string type)
+    {
+        if (!stripNamespaces) return type;
+        return namespaceRegex.Replace(type, string.Empty);
+    }
+
+    if (solutionPath is null)
+    {
+        var solutions = Directory.GetFiles(Directory.GetCurrentDirectory(), "*.sln");
+        solutionPath = solutions.FirstOrDefault();
+
+        if (solutionPath is null)
         {
-            var model = compilation.GetSemanticModel(typeDeclaration.SyntaxTree);
-            var typeSymbol = model.GetDeclaredSymbol(typeDeclaration);
+            Console.WriteLine("Solution file could not be located");
+            return;
+        }
+    }
+    
+    var sln = await workspace.OpenSolutionAsync(solutionPath);
+    foreach (var project in sln.Projects)
+    {
+        if (verbose) Console.WriteLine($"Processing {project.Name}");
+        
+        var compilation = await project.GetCompilationAsync();
 
-            var handlerInterface = typeSymbol!.Interfaces.FirstOrDefault(x =>
-                x is { Name: "IRequestHandler" or "INotificationHandler", ContainingNamespace.Name: "MediatR" });
-            
-            if (handlerInterface is not null)
+        if (compilation is null) continue;
+        
+        foreach (var tree in compilation.SyntaxTrees)
+        {
+            var root = await tree.GetRootAsync();
+            foreach (var typeDeclaration in root.DescendantNodes().OfType<ClassDeclarationSyntax>())
             {
-                var requestType = handlerInterface.TypeArguments.First().ToString();
-                var responseType = handlerInterface.TypeArguments.Length == 1
-                    ? null
-                    : handlerInterface.TypeArguments.Last().ToString();
-                handlers.Add(new(typeSymbol.ToString()!, requestType!, responseType));
-            }
-            
-            var constructors = typeDeclaration.DescendantNodes().OfType<ConstructorDeclarationSyntax>().ToList();
-            
-            if (constructors.Count != 1) continue;
-            
-            var ctor = model.GetDeclaredSymbol(constructors.First());
-            var mediatorParameter = ctor!.Parameters.FirstOrDefault(x => x.Type.Name == "IMediator");
-         
-            if (mediatorParameter is null) continue;
-            
-            var sentTypes = typeDeclaration.DescendantNodes()
-                .OfType<InvocationExpressionSyntax>()
-                .Select(x => model.GetSymbolInfo(x).Symbol)
-                .Where(x => x.ContainingNamespace.Name == "MediatR" && x.Name is "Send" or "Publish")
-                .Cast<IMethodSymbol>()
-                .Select(x => x.TypeArguments.First().ToString())
-                .ToList();
+                var model = compilation.GetSemanticModel(typeDeclaration.SyntaxTree);
+                var typeSymbol = model.GetDeclaredSymbol(typeDeclaration);
 
-            senders[typeSymbol.ToString()!] = sentTypes!;
+                var handlerInterface = typeSymbol!.Interfaces.FirstOrDefault(x =>
+                    x is { Name: "IRequestHandler" or "INotificationHandler", ContainingNamespace.Name: "MediatR" });
+                
+                if (handlerInterface is not null)
+                {
+                    var requestType = handlerInterface.TypeArguments.First().ToString();
+                    var responseType = handlerInterface.TypeArguments.Length == 1
+                        ? null
+                        : handlerInterface.TypeArguments.Last().ToString();
+                    
+                    handlers.Add(new(typeSymbol.ToString()!, requestType!, responseType));
+                }
+                
+                var constructors = typeDeclaration.DescendantNodes()
+                    .OfType<ConstructorDeclarationSyntax>().ToList();
+                
+                if (constructors.Count != 1) continue;
+                
+                var ctor = model.GetDeclaredSymbol(constructors.First());
+                var mediatorParameter = ctor!.Parameters.FirstOrDefault(x => x.Type.Name == "IMediator");
+             
+                if (mediatorParameter is null) continue;
+                
+                var sentTypes = typeDeclaration.DescendantNodes()
+                    .OfType<InvocationExpressionSyntax>()
+                    .Select(x => model.GetSymbolInfo(x).Symbol)
+                    .Where(x => x!.ContainingNamespace.Name == "MediatR" && x.Name is "Send" or "Publish")
+                    .Cast<IMethodSymbol>()
+                    .Select(x => x.TypeArguments.First().ToString())
+                    .ToList();
+
+                senders[typeSymbol.ToString()!] = sentTypes!;
+            }
+        }
+    }
+
+    foreach (var (sender, types) in senders)
+    {
+        Console.WriteLine(FormatTypeName(sender));
+        int count = 1;
+        foreach (var type in types)
+        {
+            Console.WriteLine($" {(count == types.Count ? '└' : '├')} {FormatTypeName(GetRequest(type))} -> {FormatTypeName(GetHandler(type))}");
+            count++;
         }
     }
 }
 
-Console.WriteLine("Handlers");
-foreach (var handler in handlers)
-{
-    Console.WriteLine(" - " + handler.Name);
-}
-
-foreach (var (s, m) in senders)
-{
-    Console.WriteLine("Sender " + s);
-    foreach (var mes in m)
-    {
-        Console.WriteLine(" - " + mes);
-    }
-}
